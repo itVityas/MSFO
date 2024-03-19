0371f381791b39a6e4f152bd1cb8c43debd18ab1
Vlad <foile.dm@gmail.com>
2024-03-19 16:27:50 +0300

    Added: EGIL, Bill, Store, Report and Material models

--- adjustment8/models.py
+++ adjustment8/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+class EGIL(models.Model):
+    data = models.DateField('Data')
+    month_index = models.FloatField('Month index')
+    year_index = models.FloatField('Year index')
+    start_hyper_index = models.FloatField('Start hyperinflation index')
+    hyper_index = models.FloatField('Index for hyperinflation')
+
+
+class Bill(models.Model):
+    name = models.CharField('Bill name')
+    numbers = models.IntegerField('Bill number')
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Store(models.Model):
+    name = models.CharField('Store name')
+    numbers = models.IntegerField('Store number')
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Report(models.Model):                         # ДОПИСАТЬ ЗАВТРА, Я ЗАПУТАЛСЯ
+    name = models.CharField('Report name')
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+MEASURE_CHOICES = (
+    ("шт", "шт"),
+    ("кг", "кг"),
+)
+
+
+class Material(models.Model):
+    id_bill = models.ForeignKey(Bill, on_delete=models.CASCADE)
+    id_store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    id_report = models.ForeignKey(Report, on_delete=models.CASCADE)
+    name = models.CharField('Material name')
+    code = models.IntegerField('Code material')
+    measuring = models.CharField('Measuring')
+
+    def __str__(self):
+        return f"{self.name}"
