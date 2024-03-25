5bcb17697a2051ab41cac89dcafc8b4aec50f1fa
Vlad <foile.dm@gmail.com>
2024-03-25 08:33:02 +0300

    Updated: material, report models. Added: Entrance models

--- adjustment8/models.py
+++ adjustment8/models.py
@@ -25,26 +25,41 @@ class Store(models.Model):
         return f"{self.name}"
 
 
-class Report(models.Model):                         # ДОПИСАТЬ ЗАВТРА, Я ЗАПУТАЛСЯ
+class Report(models.Model):
     name = models.CharField('Report name')
+    date_write_off = models.DateField('Date write off')
+    date_necessity = models.DateField('Date necessity')
+    date_ig2014 = models.DateField('Date IG2014')
 
     def __str__(self):
         return f"{self.name}"
-
-
-MEASURE_CHOICES = (
-    ("шт", "шт"),
-    ("кг", "кг"),
-)
 
 
 class Material(models.Model):
-    id_bill = models.ForeignKey(Bill, on_delete=models.CASCADE)
-    id_store = models.ForeignKey(Store, on_delete=models.CASCADE)
-    id_report = models.ForeignKey(Report, on_delete=models.CASCADE)
+
     name = models.CharField('Material name')
     code = models.IntegerField('Code material')
     measuring = models.CharField('Measuring')
+    article = models.IntegerField('Article')
 
     def __str__(self):
         return f"{self.name}"
+
+
+class Entrance(models.Model):
+    id_material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    id_report = models.ForeignKey(Report, on_delete=models.CASCADE)
+    id_bill = models.ForeignKey(Bill, on_delete=models.CASCADE)
+    id_store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    date = models.DateField('Receipt date')
+    count = models.FloatField('Count')
+    all_price = models.FloatField('Cost')
+    price = models.FloatField('Price')
+    reclass = models.FloatField('IFRS reclass account')
+    write_off = models.CharField('Write-off of inventories')
+    necessity_reserve = models.CharField('Necessity form reserve')
+    ig2014 = models.FloatField('IG2014')
+    cost_msfo = models.FloatField('IFRS cost')
+    write_up = models.FloatField('Unrealized GI revaluation')
+    cost_write_off = models.FloatField('Cost of materials written off')
+    reserve = models.FloatField('IFRS reserve')
