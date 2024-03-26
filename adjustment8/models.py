ef96abd8b822711a1eccee3d334a67cb268dc447
Vlad <foile.dm@gmail.com>
2024-03-26 13:50:51 +0300

    Changed: models, gitignore

--- adjustment8/models.py
+++ adjustment8/models.py
@@ -5,12 +5,12 @@ class EGIL(models.Model):
     data = models.DateField('Data')
     month_index = models.FloatField('Month index', blank=True)
     year_index = models.FloatField('Year index', blank=True)
-    start_hyper_index = models.FloatField('Start hyperinflation index', blank= True)
+    start_hyper_index = models.FloatField('Start hyperinflation index', blank=True)
     hyper_index = models.FloatField('Index for hyperinflation')
 
 
 class Bill(models.Model):
-    name = models.CharField('Bill name')
+    name = models.CharField('Bill name', max_length=255)
     numbers = models.IntegerField('Bill number')
 
     def __str__(self):
@@ -18,7 +18,7 @@ class Bill(models.Model):
 
 
 class Store(models.Model):
-    name = models.CharField('Store name')
+    name = models.CharField('Store name', max_length=255)
     numbers = models.IntegerField('Store number')
 
     def __str__(self):
@@ -26,7 +26,7 @@ class Store(models.Model):
 
 
 class Report(models.Model):
-    name = models.CharField('Report name')
+    name = models.CharField('Report name', max_length=255)
     date_write_off = models.DateField('Date write off')
     date_necessity = models.DateField('Date necessity')
     date_ig2014 = models.DateField('Date IG2014')
@@ -37,9 +37,9 @@ class Report(models.Model):
 
 class Material(models.Model):
 
-    name = models.CharField('Material name')
+    name = models.CharField('Material name', max_length=255)
     code = models.IntegerField('Code material')
-    measuring = models.CharField('Measuring')
+    measuring = models.CharField('Measuring', max_length=255)
     article = models.IntegerField('Article')
 
     def __str__(self):
@@ -56,8 +56,8 @@ class Entrance(models.Model):
     all_price = models.FloatField('Cost')
     price = models.FloatField('Price')
     reclass = models.FloatField('IFRS reclass account')
-    write_off = models.CharField('Write-off of inventories')
-    necessity_reserve = models.CharField('Necessity form reserve')
+    write_off = models.CharField('Write-off of inventories', max_length=255)
+    necessity_reserve = models.CharField('Necessity form reserve', max_length=255)
     ig2014 = models.FloatField('IG2014')
     cost_msfo = models.FloatField('IFRS cost')
     write_up = models.FloatField('Unrealized GI revaluation')
