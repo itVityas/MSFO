850088421c9826fdba2350bbee6deb7e0908f37a
Vlad <foile.dm@gmail.com>
2024-03-27 11:39:03 +0300

    Changed: IG2014 functions

--- adjustment8/IG2014.py
+++ adjustment8/IG2014.py
@@ -1,4 +1,5 @@
 from adjustment8.models import EGIL
+from openpyxl import load_workbook
 
 
 def read_ig2014(line: int, workbook):
@@ -14,7 +15,8 @@ def read_ig2014(line: int, workbook):
     return date_ig, month_index_ig, year_index_ig, start_hyper_index_ig, hyper_index_ig, flag_ig
 
 
-def write_ig2014():
+def write_ig2014(path):
+    wb = load_workbook(f'{path}', read_only=True, data_only=True)
     x = 7
     flag = True
     while flag:
