cb916614e6f8da831fd1b19256c9a2fa201341e3
Vlad <foile.dm@gmail.com>
2024-04-03 10:45:20 +0300

    Changed: excel_utils worked version

--- adjustment8/excel_utils.py
+++ adjustment8/excel_utils.py
@@ -8,24 +8,18 @@ class CellException(Exception):
     pass
 
 
-def get_or_create_bill(path):
-    workbook = load_workbook(f'{path}', read_only=True, data_only=True)
-    wb_list = workbook.active
+def get_or_create_bill(wb_list):
     name = wb_list['A14'].value
     id_bill, created = Bill.objects.get_or_create(name=name)
-    workbook.close()
     return id_bill
 
 
-def get_or_create_store(line: int, path):
-    workbook = load_workbook(f'{path}', read_only=True, data_only=True)
-    wb_list = workbook.active
+def get_or_create_store(line: int, wb_list):
     name = wb_list[f'A{line}'].value
     numbers = wb_list[f'C{line}'].value
     if numbers is None:
         raise CellException
     id_store, created = Store.objects.get_or_create(name=name, numbers=numbers)
-    workbook.close()
     return id_store
 
 
@@ -44,27 +38,20 @@ def get_report(name):
     return id_report, date_write_off, date_necessity, date_ig2014
 
 
-def read_material(line: int, path):
-    workbook = load_workbook(f'{path}', read_only=True, data_only=True)
-    wb_list = workbook.active
+def read_material(line: int, wb_list):
     name = wb_list[f'A{line}'].value
     code = wb_list[f'C{line}'].value
     measuring = wb_list[f'D{line}'].value
-    article = wb_list[f'E{line}'].value
-    workbook.close()
-    return name, code, measuring, article
+    return name, code, measuring
 
 
-def read_date(line: int, path):
-    workbook = load_workbook(f'{path}', read_only=True, data_only=True)
-    wb_list = workbook.active
+def read_date(line: int, wb_list):
     date = wb_list[f'A{line}'].value
     date = date_convert(date)                       # Index error if not correct cell
     all_price = wb_list[f'H{line}'].value
     if all_price is None:
         all_price = 0
     count = wb_list[f'H{line + 1}'].value
-    workbook.close()
     if (all_price or count) is None:
         raise CellException
     return date, all_price, count
@@ -123,27 +110,31 @@ def calculate_ig2014(date):
 
 
 def write_all_date(path, report_name):
+    print(f'Start func:{datetime.datetime.now()}')
+    workbook = load_workbook(f'{path}', data_only=True)
+    print(f'Finish load WB:{datetime.datetime.now()}')
+    wb_list = workbook.active
     line = 16
     id_report, date_write_off, date_necessity, date_ig2014 = get_report(report_name)
-    id_bill = get_or_create_bill(path)
+    id_bill = get_or_create_bill(wb_list)
     flag_bill = True
     while flag_bill:
         try:
-            id_store = get_or_create_store(line, path)
+            id_store = get_or_create_store(line, wb_list)
         except CellException:
             break
         line += 2
         while True:
-            name, code, measuring, article = read_material(line, path)
-            if article is None:
+            name, code, measuring = read_material(line, wb_list)
+            if measuring is None:
                 break
             id_material, created = Material.objects.get_or_create(name=name, code=code,
-                                                                  measuring=measuring, article=article)
+                                                                  measuring=measuring)
             line += 2
             flag_material = True
             while flag_material:
                 try:
-                    date, all_price, count = read_date(line, path)
+                    date, all_price, count = read_date(line, wb_list)
                     line += 2
                     (price, write_off, reclass, necessity_reserve, ig2014, cost_msfo, write_up, cost_write_off,
                      reserve) = (entrance_calculation(date, all_price, count, date_write_off, date_necessity,
@@ -160,6 +151,8 @@ def write_all_date(path, report_name):
                 except CellException:
                     line += 2
                     continue
+    workbook.close()
+    print(f'Finish func:{datetime.datetime.now()}')
     return
 
 
@@ -167,4 +160,5 @@ create_report(name='2023', date_write_off=datetime.date(2013, 12, 31),
               date_necessity=datetime.date(2020, 12, 31),
               date_ig2014=datetime.date(2015, 1, 1))
 Material.objects.all().delete()
+Store.objects.all().delete()
 write_all_date('/home/foile/MSFO/MSFO/static/xlsx/test1001.xlsx', '2023')
