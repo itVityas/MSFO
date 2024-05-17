057615092dccb1e75a8f9eedd30ee6c64e4ee5cc
AntonKralinWork <kralin.dart@gmai.com>
2024-05-17 14:23:01 +0300

    Add MySQL

--- MSFO/MSFO/settings.py
+++ MSFO/MSFO/settings.py
@@ -76,13 +76,22 @@ WSGI_APPLICATION = 'MSFO.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'msfo',
+        'USER': 'msfo',
+        'PASSWORD': 'gfhjkm2@',
+        'HOST': 'bd',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
