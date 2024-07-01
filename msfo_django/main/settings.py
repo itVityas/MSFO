fa53eb265936c345bb9c63633cd3ac7237d9c1a4
Vlad <foile.dm@gmail.com>
2024-07-01 13:03:30 +0300

    Delete not used static path

--- msfo_django/main/settings.py
+++ msfo_django/main/settings.py
@@ -131,7 +131,6 @@ STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    "msfo_django/static/",
 ]
 
 # Default primary key field type
