6ee75a794eccd6f5ede732f673cc5bfa4f79c478
Vlad <foile.dm@gmail.com>
2024-11-26 13:49:07 +0300

    Change models

--- msfo_django/msfo1/models.py
+++ msfo_django/msfo1/models.py
@@ -10,7 +10,7 @@ class Counterparty(models.Model):
 
 class AccountMapping(models.Model):
     account_1c = models.CharField(max_length=50)
-    sorting_number = models.IntegerField()
+    sorting_number = models.CharField(max_length=50)
     db_account_number = models.CharField(max_length=50)
 
     class Meta:
@@ -23,23 +23,9 @@ class AccountMapping(models.Model):
 
 class Debt(models.Model):
     counterparty = models.ForeignKey(Counterparty, on_delete=models.CASCADE)
-    account_1c = models.CharField(max_length=10)
-    sorting_number = models.CharField(max_length=10)
-    report_account = models.CharField(max_length=50, blank=True)
+    account = models.ForeignKey(AccountMapping, on_delete=models.CASCADE)
     debt_byn = models.DecimalField(max_digits=15, decimal_places=2)
     debt_contract_currency = models.DecimalField(max_digits=15, decimal_places=2)
     contract_currency = models.CharField(max_length=10)
     date_of_debt = models.DateField()
     payment_term_days = models.IntegerField()
-
-    def save(self, *args, **kwargs):
-        try:
-            mapping = AccountMapping.objects.get(
-                account_1c=self.account_1c,
-                sorting_number=self.sorting_number
-            )
-            self.db_account_number = mapping.db_account_number
-        except AccountMapping.DoesNotExist:
-            self.db_account_number = 'Unknown'
-
-        super().save(*args, **kwargs)
