95932d58395c272098a403a54fddc4b617b687c3
Vlad <foile.dm@gmail.com>
2024-12-20 14:52:21 +0300

    Add currency rate in celery task

--- msfo_django/msfo1/tasks.py
+++ msfo_django/msfo1/tasks.py
@@ -1,4 +1,4 @@
-from msfo1.utils.util import pull_all_accounts
+from msfo1.utils.util import pull_all_accounts, update_currency_rates_for_year
 from msfo1.utils.excel_write import generate_msfo_report
 from celery import shared_task
 
@@ -6,5 +6,6 @@ from celery import shared_task
 @shared_task()
 def crete_report_task(year_report: int):
     report_file = pull_all_accounts(year_report)
+    update_currency_rates_for_year(year_report)
     generate_msfo_report(year=year_report, report_file=report_file)
 
