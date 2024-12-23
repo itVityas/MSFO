d296e79c96b0bdccc6dff468129edd91441a8261
Vlad <foile.dm@gmail.com>
2024-12-23 09:52:56 +0300

    Change celery task. Now it update all account each time

--- msfo_django/msfo1/tasks.py
+++ msfo_django/msfo1/tasks.py
@@ -1,10 +1,11 @@
-from msfo1.utils.util import pull_all_accounts, update_currency_rates_for_year
+from msfo1.utils.util import pull_all_accounts, update_currency_rates_for_year, populate_account_mappings
 from msfo1.utils.excel_write import generate_msfo_report
 from celery import shared_task
 
 
 @shared_task()
 def crete_report_task(year_report: int):
+    populate_account_mappings()
     report_file = pull_all_accounts(year_report)
     update_currency_rates_for_year(year_report)
     generate_msfo_report(year=year_report, report_file=report_file)
